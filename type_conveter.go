a36e7d921078ab7eecdbca9945eddb44f1451155
Sohel Mia <sohelcse1999@gmail.com>
2023-02-04 00:28:26 +0600

    add boolean conveter

--- type_conveter.go
+++ type_conveter.go
@@ -10,3 +10,17 @@ func String(v string) *string {
 func StringValue(v *string) string {
 	return *v
 }
+
+// `Bool` functions returns a pointer to the bool value passed in.
+func Bool(v bool) *bool {
+	return &v
+}
+
+// `BoolValue` returns the value of the bool pointer passed in or
+// false if the pointer is nil.
+func BoolValue(v *bool) bool {
+	if v != nil {
+		return *v
+	}
+	return false
+}
--- type_conveter_test.go
+++ type_conveter_test.go
@@ -7,6 +7,30 @@ import (
 )
 
 func TestStringValueToPointer(t *testing.T) {
-	assert.Equal(t, 123, 123)
+	strValue := "Hello Gotipath"
+	prtStr := String(strValue)
+	assert.Equal(t, &strValue, prtStr)
+}
 
+func TestStringPointerToValueString(t *testing.T) {
+	expValue := "Hello Gotipath"
+
+	value := StringValue(&expValue)
+
+	assert.Equal(t, expValue, value)
+
+}
+
+func TestBooleanToPointerBoolean(t *testing.T) {
+	t.Run("it boolean on true", func(t *testing.T) {
+		value := true
+		output := Bool(value)
+		assert.Equal(t, &value, output)
+	})
+
+	t.Run("it boolean on false", func(t *testing.T) {
+		value := false
+		output := Bool(value)
+		assert.Equal(t, &value, output)
+	})
 }
